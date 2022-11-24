1503894cbef0e742a53a4ba9a0e97250063d92dd
David Alen <davidalen.dev@gmail.com>
2022-11-23 22:06:23 -0300

    add: idk has been a month since this change

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -3,15 +3,18 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
 )
 
 func main() {
 	// handle route using handler function
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("New request at", time.Now(), "Request URI is: ", r.RequestURI)
+
 		fmt.Fprintf(w, "Welcome to new server!")
 	})
 
-	fmt.Println("Server is ready on :5000")
+	fmt.Println("Server is ready on :5050")
 
 	// listen to port
 	http.ListenAndServe(":5050", nil)
